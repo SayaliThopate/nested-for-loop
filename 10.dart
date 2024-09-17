12e8b0e06cd8975513b3eb838a2df9dce9b1300e
SayaliThopate <158029728+SayaliThopate@users.noreply.github.com>
2024-09-17 08:54:01 +0530

    Update 10.dart

--- 10.dart
+++ 10.dart
@@ -1,21 +1,19 @@
 import 'dart:io';
+void main(){
+  int n=6;
+  int m=1;
+  for (int i=1;i<=n;i++){
+    int temp=n-i+1;
+    for(int j=1;j<=i;j++){
+      if(j%2==0){
+        stdout.write("$m\t");
+        m++;
 
-void main() {
-  int n = 6;  
-
-  for (int i = 1; i <= n; i++) {
-    int even = i;  
-    int odd = n - i + 1;  
-    
-    for (int j = 1; j <= i; j++) {
-      if (j % 2 == 0) {
-        stdout.write("$even ");
-        even++;  
-      } else {
-        stdout.write("$odd ");
-        odd++;  
+      }else{
+        stdout.write("$temp\t");
+        temp++;
       }
     }
-    print("");  
+    print("");
   }
 }
